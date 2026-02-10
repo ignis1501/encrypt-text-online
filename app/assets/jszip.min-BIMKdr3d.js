c714c31e77dd1a8c19e4416f96ba13edf94bc584
forja <laforjadetronheim@gmx.com>
2026-02-10 10:05:20 +0000

    fix(bug imatges) => problemes ruta relativa imatges

---
+++ app/assets/jszip.min-BIMKdr3d.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-Do1wyrDC.js";import{r as e}from"./jszip.min-Bw1r15dl.js";var o=e();const t=r(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));export{s as j};
