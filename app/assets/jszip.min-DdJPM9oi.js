2208cf7221db3bf189908471fc9c716cf0e3bcbe
forja <laforjadetronheim@gmx.com>
2026-02-10 10:40:26 +0000

    fix => rutes imatges

---
+++ app/assets/jszip.min-DdJPM9oi.js
@@ -0,0 +1,1 @@
+import{a as r}from"./index-DzGC2bZm.js";import{r as e}from"./jszip.min-Do7N7k8k.js";var o=e();const t=r(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));export{s as j};
